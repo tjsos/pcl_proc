6286a03fda8454100f68776fb1687f0d68a73d92
tjsos <tony.jacob@uri.edu>
2024-01-26 16:08:22 -0500

    probably some projection issue

--- scripts/path.py
+++ scripts/path.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import PoseStamped
 import numpy as np
 import cv2
 import math
+import scipy
 
 class CostMapProcess:
     def __init__(self) -> None:
@@ -26,7 +27,6 @@ class CostMapProcess:
         w = message.pose.pose.orientation.w
 
         self.yaw = math.atan2(2 * (w * z + x * y), 1 - 2 * (y**2 + z**2))
-        # print(self.yaw)
 
     def mapCB(self, message):
         """
@@ -63,45 +63,104 @@ class CostMapProcess:
         Edges
         """
         #Get Edge
-        canny = cv2.Canny(dilate,200,255)
+        canny_image = cv2.Canny(dilate,200,255)
         #Find cordinates of the edges
-        pixels = self.find_cordinates_max_value(canny)
+        raw_pixels = self.find_cordinates_max_value(canny_image)
         #Get usable edges
-        cell_row, cell_row_polar = self.get_edges(pixels)
+        edge, edge_polar = self.get_edges(raw_pixels)
         #Edges in vx_frame
-        vx_frame_edges = self.vx_frame_tf(cell_row_polar)
+        vx_frame_edges = self.odom_to_vx_frame_tf(edge_polar)
 
         #//To-Do : Fit line. Upsample?
+        #plot vx frame
+        vx_frame_image = self.plot_circles_vx_frame(vx_frame_edges)
+        
+        path_cells, path_cells_polar = self.curve_fit(vx_frame_image)
+        odom_frame_path = self.vx_to_odom_frame_tf(path_cells_polar)
+        # print(odom_frame_path)
         """
         Path
         """
-        path = self.pub_path(cell_row)
-        self.pub.publish(path)
+        self.pub_path(odom_frame_path)
 
 
         """
         Visualization
         """
-        #plot in odom frame
-        edge = self.plot_circles(cell_row)
-        #plot vx frame
-        vx_frame = self.plot_circles_vx_frame(vx_frame_edges)
-        #Compare the Canny and Custom
-        compare = self.compare_canny_edge(pixels, cell_row)
+        ## plot in odom frame
+        # edge_image = self.plot_circles(edge)
+        ## Compare the Canny and Custom
+        compare = self.compare_two_lists(raw_pixels, odom_frame_path)
 
-        mix= np.hstack((data, canny, compare, vx_frame))
+        mix= np.hstack((data, canny_image, compare, vx_frame_image))
         cv2.imshow("window", mix)
         cv2.waitKey(1)
+    
+    def model_f(self,x, a, b, c, d):
+        # return a*(x-b)**2 + c
+        return a*x**3 + b* x**2 +c*x +d
+        
+    def curve_fit(self, vx_frame):
+        #Only interested in path frame
+        vx_frame_cropped = vx_frame[ :3*self.width//4, self.height//2:]
+        img_cropped_cart = self.find_cordinates_max_value(vx_frame_cropped)
+        if img_cropped_cart is not []:
+            x_coordinates, y_coordinates = zip(*img_cropped_cart)
+            try:
+                #FIT THE CURVE
+                p_opt, p_cov = scipy.optimize.curve_fit(self.model_f, x_coordinates, y_coordinates)
+                a_opt, b_opt, c_opt, d_opt = p_opt
+                #GET LINE
+                x_model = np.linspace(min(x_coordinates), max(x_coordinates), 20)
+                y_model = self.model_f(x_model, a_opt, b_opt, c_opt,d_opt)
+                #Y here is X in Image frame
+                cordinates_list_model = list(zip(y_model, x_model))
+                cordinates_list_model = [list(coord) for coord in cordinates_list_model]
 
-    def vx_frame_tf(self, polar_list):
+                #Shift back to image frame from cropped image
+                sampled_coordinates = [[round(x)+ self.height//2, round(y)] for x, y in cordinates_list_model]
+                
+                # Shift coordinates to center of the image
+                shifted_coordinates = [[x - self.height//2, self.width//2 - y] for x, y in sampled_coordinates]
+
+                # Convert shifted Cartesian coordinates to polar coordinates
+                polar_coordinates = [[np.sqrt(x**2 + y**2), np.arctan2(x, y)]for x, y in shifted_coordinates]
+                
+                return sampled_coordinates, polar_coordinates
+
+            except RuntimeError:
+                print("No solution found")
+                return [],[]
+
+
+    def vx_to_odom_frame_tf(self, polar_list):
+        """
+        Transfer it to odom frame
+        """
+        vx_theta = self.yaw
+        for points in polar_list:
+             
+            if round(vx_theta) <= 0:
+                points[1] = self.roll_over_radians( -points[1] + vx_theta)
+            elif round(vx_theta) > 0:
+                points[1] = self.roll_over_radians( points[1] - vx_theta)
+
+        # print(points[1])
+        cartesian_coordinates = [[abs(round(r * np.sin(theta))), round(r * np.cos(theta))] for r, theta in polar_list]
+        cartesian_coordinates = [[int(self.height//2 + x),int(self.width//2 - y),] for x, y in cartesian_coordinates]
+        # cartesian_coordinates = sorted(cartesian_coordinates, key=lambda coord: coord[1], reverse=True)
+
+        return cartesian_coordinates
+
+    def odom_to_vx_frame_tf(self, polar_list):
         """
         Transfer it to vehicle frame
         """
         yawd_p = [list(t) for t in polar_list]
 
-        theta = self.yaw
+        vx_theta = self.yaw
         for points in yawd_p:
-            points[1] = self.roll_over_radians(points[1] + np.float64(theta))
+            points[1] = self.roll_over_radians(points[1] + np.float64(vx_theta))
 
        # Step 4: Convert to Cartesian.
         cartesian_coordinates = [[round(r * np.sin(theta)), round(r * np.cos(theta))] for r, theta in yawd_p]
@@ -117,7 +176,7 @@ class CostMapProcess:
 
         # Get the coordinates of pixels with the maximum intensity
         max_intensity_coordinates = np.column_stack(np.where(image == max_intensity))
-
+        #returns in [x,y]
         return max_intensity_coordinates
     
     def roll_over_radians(self, angle, range_start=-math.pi, range_end=math.pi):
@@ -160,7 +219,8 @@ class CostMapProcess:
         #     diff = len(cartesian_coordinates) - self.width//4
         #     for i in range(diff):
         #         cartesian_coordinates.pop(i)
-        
+        #
+        # [x,y] [r,theta]
         return cartesian_coordinates, polar_coordinates
 
     def plot_circles(self, coordinates_list, radius=1, color=155):
@@ -186,12 +246,17 @@ class CostMapProcess:
         for coordinates in coordinates_list:
             center = tuple(coordinates)
             cv2.circle(image, center, radius, color, 1)
-        image = cv2.line(image, (self.width//2, self.height//2), (self.width//2, self.height//2-10), 200, 1)
-        image = cv2.line(image, (self.width//2, self.height//2), (self.width//2-10, self.height//2), 200, 1) 
+
+        image = cv2.line(image, (0, 3*self.width//4), (200, 3*self.width//4), 100, 1)
+        image = cv2.line(image, (100, 0), (100, 200), 100, 1)
+
+        #Draw Vx frame
+        # image = cv2.line(image, (self.width//2, self.height//2), (self.width//2, self.height//2-5), 200, 1)
+        # image = cv2.line(image, (self.width//2, self.height//2), (self.width//2-5, self.height//2), 200, 1) 
 
         return image
     
-    def compare_canny_edge(self, list1, list2):
+    def compare_two_lists(self, list1, list2):
         """
         Viz function to plot the original canny and extracted edge
         """
@@ -211,7 +276,7 @@ class CostMapProcess:
     
     def pub_path(self, shifted_coordinates):
         """
-        Create the path message
+        Create and send the path message
         """
         path = Path()
         
@@ -223,7 +288,7 @@ class CostMapProcess:
             pose_stamped.header.frame_id = self.frame
             pose_stamped.header.stamp = self.time
 
-            #IMAGE TO MAP FRAME
+            #IMAGE TO MAP FRAME FIXED TO ODOMs
             x = -((cords[1] - self.height//2) * self.resolution) + self.vx_x
             y = ((self.width//2 - cords[0])* self.resolution ) + self.vx_y
 
@@ -235,7 +300,7 @@ class CostMapProcess:
             pose_stamped.pose.orientation.w = 1
             path.poses.append(pose_stamped)
 
-        return path
+        self.pub.publish(path)
     
     
 if __name__ == "__main__":
